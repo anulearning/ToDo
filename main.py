8d7c9220119962ea63bb3dbc79612fc04c638e1b
anuradha <anulearning3@gmail.com>
2021-11-06 09:42:54 -0400

    Moved styles from helpers.py to mytodo.kv Added logic for adding showing tasks in the list

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-# from kivymd.uix.list import MDList,OneLineListItem  list
-# from kivy.uix.scrollview import ScrollView        list
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.widget import Widget
+#from kivymd.uix.list import MDList,OneLineListItem
+#from kivy.uix.scrollview import ScrollView
 from kivymd.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from helpers import ToDo_helper
-from helpers import Add_helper
-from helpers import High_helper
 
+
+# from helpers import ToDo_helper
+# from helpers import Add_helper
+# from helpers import High_helper
 
 
 class MyToDo(MDApp):
@@ -15,32 +18,32 @@ class MyToDo(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Amber"
         self.theme_cls.primary_hue = "A700"
-        screen = MDScreen()
+       # screen = MDScreen()
 
-       # scroll = ScrollView()      list
-      #  list_view = MDList()       list
-       # item1 = OneLineListItem(text = "Item 1")       list
-       # builder.load_string('mytodo.kv')
-        text_ToDo = Builder.load_string(ToDo_helper)
-        btn_Add = Builder.load_string(Add_helper)
-        btn_High = Builder.load_string(High_helper)
+        # scroll = ScrollView()
+        # list_view = MDList()
+        # item1 = OneLineListItem(text = "Item 1")
+        screen = Builder.load_file('mytodo.kv')
+        # text_ToDo = Builder.load_string(ToDo_helper)
+        # btn_Add = Builder.load_string(Add_helper)
+        #  btn_High = Builder.load_string(High_helper)
 
-       # scroll.add_widget(list_view)       list
-       # list_view.add_widget(item1)        kist
-     #   screen.add_widget(scroll)      list
-        screen.add_widget(text_ToDo)
-        screen.add_widget(btn_Add)
-        screen.add_widget(btn_High)
+        # scroll.add_widget(list_view)
+        # list_view.add_widget(item1)
+        # screen.add_widget(scroll)
+        # screen.add_widget(text_ToDo)
+        # screen.add_widget(btn_Add)
+        # screen.add_widget(btn_High)
 
         return screen
 
-    def add_task_high(self):
-        print("Save the task as severity 1 in the DB and show in list")
-      #  mylist = OneLineListItem(text=self.dialog.content_cls.textfield.text)
 
-       # self.list_view.add_widget(mylist)
 
-    def call(self,obj):
+    # mylist = OneLineListItem(text=self.ToDo_helper.textfield.text)
+
+    # self.list_view.add_widget(mylist)
+
+    def call(self, obj):
         if obj.icon == "priority-high":
             self.add_task_high()
         elif obj.icon == "priority-low":
@@ -48,6 +51,13 @@ class MyToDo(MDApp):
         else:
             print("Mark the task as done in the DB")
 
+    def add_task_high(self):
+        print("Save the task as severity 1 in the DB and show in list")
+        print(self.root.ids.textfield.text)
+        items = self.root.ids.textfield.text
+        self.root.ids.list.text = items
+        self.root.ids.textfield.text = ""
+       # self.root.ids.but.close()
 
-
-MyToDo().run()
+if __name__ == "__main__":
+    MyToDo().run()
